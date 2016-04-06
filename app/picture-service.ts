7a2a826f6d46d681b7e09f69f65eeabb13d7601d
Hollannikas <hollannikas@users.noreply.github.com>
2016-04-06 20:56:39 +0300

    bookmarkable picture page and comments from backend

--- app/picture-service.ts
+++ app/picture-service.ts
@@ -5,6 +5,13 @@ export class Picture {
   private id:string;
   private caption:string;
   private filename:string;
+  private comments:Comment[];
+}
+
+export class Comment {
+  private author:string;
+  private text:string;
+  private date:string;
 }
 
 @Injectable()
@@ -14,6 +21,36 @@ export class PictureService {
   private requestoptions:RequestOptions;
   
   constructor(private http:Http) {
+  }
+
+  /**
+   * Retrieves picture from service
+   * @returns {Promise<Picture>}
+   */
+  getPicture(id:string):Promise<Picture> {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json; charset=UTF-8');
+    this.headers.append('Accept', 'application/json; charset=UTF-8');
+
+    this.requestoptions = new RequestOptions({
+      method: RequestMethod.Get,
+      url: 'http://localhost:8080/rest/pictures/' + id + '/metadata',
+      headers: this.headers,
+    })
+
+    return new Promise((resolve, reject)=> {
+      this.http.request(new Request(this.requestoptions)).subscribe(
+        (response:Response) => {
+          const json = response.json();
+          if (json.error) {
+            reject(json);
+          }
+          resolve(json);
+        },
+        () => {
+          reject({error: null});
+        });
+    });
   }
 
   /**
@@ -28,6 +65,39 @@ export class PictureService {
     this.requestoptions = new RequestOptions({
       method: RequestMethod.Get,
       url: 'http://localhost:8080/rest/pictures/',
+      headers: this.headers,
+    })
+
+    return new Promise((resolve, reject)=> {
+      this.http.request(new Request(this.requestoptions)).subscribe(
+        (response:Response) => {
+          const json = response.json();
+          if (json.error) {
+            reject(json);
+          }
+          resolve(json);
+        },
+        () => {
+          reject({error: null});
+        });
+    });
+  }
+
+  /**
+   * Saves a comments to a picture
+   * @param pictureId
+   * @param author
+   * @param text
+   * @returns {Promise<T>|Promise<R>|Promise}
+     */
+  saveComment(pictureId:string, author:string, text:string) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/json; charset=UTF-8');
+    this.headers.append('Accept', 'application/json; charset=UTF-8');
+
+    this.requestoptions = new RequestOptions({
+      method: RequestMethod.Get,
+      url: 'http://localhost:8080/rest/pictures/' + pictureId + '/comments/add/' + author + '/' + text,
       headers: this.headers,
     })
 
--- app/picture.component.ts
+++ app/picture.component.ts
@@ -3,18 +3,37 @@
  */
 import {Component} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
+import {PictureService, Picture} from './picture-service';
+import {DatePipe} from "angular2/common";
+import {Collapse} from 'ng2-bootstrap'
 
 @Component({
   selector: 'picture',
   templateUrl: 'app/picture.component.html',
+  directives: [Collapse],
+  pipes: [DatePipe],
+  providers: [PictureService]
 })
 
 export class PictureComponent {
   private id:string;
+  private picture:Picture;
+  private isCollapsed:boolean = false;
+
   private comments:string[] = ['First!', 'Second comment.. bla bla bla '];
 
-  constructor(routeParams:RouteParams) {
+  constructor(routeParams:RouteParams, private pictureservice:PictureService) {
     this.id = routeParams.get('id');
-    console.log('id = ' + this.id);
+  }
+
+  ngOnInit() {
+    this.update();
+  }
+
+  update() {
+    this.pictureservice.getPicture(this.id).then((_picture:Picture) => {
+      this.picture = _picture;
+      console.log(_picture)
+    });
   }
 }
